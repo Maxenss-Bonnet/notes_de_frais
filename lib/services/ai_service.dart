9772fc1da9e9af27ad1344b81cbebd34e8eb1070
IMaxenceI <m.bonnet@eleve.leschartreux.net>
2025-07-08 13:48:19 +0200

    api not lite

--- lib/services/ai_service.dart
+++ lib/services/ai_service.dart
@@ -11,7 +11,7 @@ class AiService {
 
   AiService()
       : _model = GenerativeModel(
-    model: 'gemini-1.5-flash',
+    model: 'gemini-2.5-flash',
     apiKey: dotenv.env['GEMINI_API_KEY']!,
   );
 
